66d5b30209df825cee0a1ed15e8b91f2ebd5f57e
Alexander Urizar <urizaraj@gmail.com>
2018-02-09 17:59:33 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,16 @@
+def sort_array_asc(numbers)
+  numbers.sort
+end
 
+def sort_array_desc(numbers)
+  result = numbers.sort do |a, b|
+    b <=> a
+  end
+  result
+end
+
+def sort_array_char_count(array)
+  result = array.sort do |a, b|
+    a.size <=> b.size
+  end
+end
